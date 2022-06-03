a8f3d6740c5cba61ea24b421e131a4e6f2603390
IAFEnvoy <tanksheng545@126.com>
2022-06-03 22:45:45 +0800

    添加彩蛋

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ window.onload = () => {
   loadTable();
   loadMeme();
   clock();
+  loadConsoleMessage();
 }
 
 const loadTable = async () => {
@@ -88,7 +89,39 @@ const openFanPage = () => {
   window.open(url, '_blank');
 }
 
-const openUpdateLog=()=>{
+const openUpdateLog = () => {
   let url = document.baseURI.replace('index.html', '').replace('index', '') + 'info.html?UpdateLog';
   window.open(url, '_blank');
+}
+
+const loadConsoleMessage = () => {
+  console.log("                            _ooOoo_ ");
+  console.log("                           o8888888o ");
+  console.log("                           88\". \"88 ");
+  console.log("                           (| -_- |) ");
+  console.log("                            O\\ = /O ");
+  console.log("                        ____/`---'\\____ ");
+  console.log("                      .   ' \\\\| |// `. ");
+  console.log("                       / \\\\||| 1 |||// \\ ");
+  console.log("                     / _||||| -9- |||||- \\ ");
+  console.log("                       | | \\\\\\ 9 /// | | ");
+  console.log("                     | \\_| ''\\-8-/'' | | ");
+  console.log("                      \\ .-\\__ `0` ___/-. / ");
+  console.log("                   ___`. .' /--2--\\ `. . __ ");
+  console.log("                .'' '< `.___\\_<0>_/___.' >'''. ");
+  console.log("               | | : `- \\`.;`\\ 2 /`;.`/ - ` : | | ");
+  console.log("                 \\ \\ `-. \\_ __\\ /__ _/ .-` / / ");
+  console.log("         ======`-.____`-.___\\_____/___.-`____.-'====== ");
+  console.log("                            `=---=' ");
+  console.log("         ............................................. ");
+  console.log("                  佛祖保佑                  永无BUG ");
+  console.log("          佛曰: ");
+  console.log("                  写字楼里写字间，写字间里程序员； ");
+  console.log("                  程序人员写程序，又拿程序换酒钱。 ");
+  console.log("                  酒醒只在网上坐，酒醉还来网下眠； ");
+  console.log("                  酒醉酒醒日复日，网上网下年复年。 ");
+  console.log("                  但愿老死电脑间，不愿鞠躬老板前； ");
+  console.log("                  奔驰宝马贵者趣，公交自行程序员。 ");
+  console.log("                  别人笑我忒疯癫，我笑自己命太贱； ");
+  console.log("                  不见满街漂亮妹，哪个归得程序员？ ");
 }
