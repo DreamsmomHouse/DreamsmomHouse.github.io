9b672f013228dc84bd007ea5fe4e26e03ad52b44
IAFEnvoy <tanksheng545@126.com>
2022-08-04 00:12:28 +0800

    突然发现info的解析炸了，感觉修掉

--- js/info.js
+++ js/info.js
@@ -1,14 +1,17 @@
-window.onload = async () => {
+window.onload = function () {
   let searchStr = location.search;
   searchStr = searchStr.substring(1);
   let searchs = searchStr.split("&");
   let category = searchs[0];
   let id = searchs[1];
   let url = id == undefined ? `DataBase/${category}.txt` : `DataBase/${category}/${id}.txt`;
-  const a = await fetch(url).then(response => {
-    if (response.status == 200)
-      document.getElementById("data").innerHTML = new marked.Parser().parse(new marked.Lexer().lex(response.text()));
+  let request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.send(null);
+  request.onloadend = function () {
+    if (request.status == 200)
+      document.getElementById("data").innerHTML = new marked.Parser().parse(new marked.Lexer().lex(request.responseText));
     else
       alert("这里还没有写完哦~");
-  })
+  }
 }
