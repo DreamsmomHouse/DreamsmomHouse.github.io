a54eef0186e0f698bdebcc306e045c8e5da368f0
IAFEnvoy <tanksheng545@126.com>
2023-08-21 00:54:40 +0800

    add some achievements

--- game/achievement.js
+++ game/achievement.js
@@ -33,5 +33,45 @@ const achievements = [
         name: '开启你的考古之旅',
         detail: '找到一个可疑的方块',
         judge: (config) => config.brush_sand + config.brush_gravel >= 1
+    }, {
+        id: 1,
+        name: '考古之路I',
+        detail: '挖掘10个方块',
+        judge: (config) => config.brush_sand + config.brush_gravel >= 10
+    }, {
+        id: 2,
+        name: '考古之路II',
+        detail: '挖掘50个方块',
+        judge: (config) => config.dig_gravel + config.dig_sand >= 50
+    }, {
+        id: 3,
+        name: '考古之路III',
+        detail: '挖掘100个方块',
+        judge: (config) => config.dig_gravel + config.dig_sand >= 100
+    }, {
+        id: 4,
+        name: '考古之路IV',
+        detail: '挖掘300个方块',
+        judge: (config) => config.dig_gravel + config.dig_sand >= 300
+    }, {
+        id: 5,
+        name: '收藏之路I',
+        detail: '清理10个可疑的方块',
+        judge: (config) => config.brush_sand + config.brush_gravel >= 10
+    }, {
+        id: 6,
+        name: '收藏之路II',
+        detail: '清理50个可疑的方块',
+        judge: (config) => config.brush_sand + config.brush_gravel >= 50
+    }, {
+        id: 7,
+        name: '收藏之路III',
+        detail: '清理100个可疑的方块',
+        judge: (config) => config.brush_sand + config.brush_gravel >= 100
+    }, {
+        id: 8,
+        name: '收藏之路IV',
+        detail: '清理300个可疑的方块',
+        judge: (config) => config.brush_sand + config.brush_gravel >= 300
     }
 ]
